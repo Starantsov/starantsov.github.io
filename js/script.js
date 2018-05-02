9177484750c5cfa72cbf1240e1449ffa6bf30c27
Starantsov <nazarstarantsov@gmail.com>
2018-05-02 21:14:05 +0300

    Project BarberShop (not finished)

---
+++ js/script.js
@@ -0,0 +1,31 @@
+var navMain = document.querySelector(".main-nav");
+var navToggle = document.querySelector(".main-nav__toggle");
+var newsOpenAll = document.querySelector(".news__to-all");
+var sectionNews = document.querySelector(".news__items");
+
+navMain.classList.remove('main-nav--nojs');
+sectionNews.classList.remove('news__items--nojs');
+
+navToggle.addEventListener("click", function(){
+    if(navMain.classList.contains("main-nav--closed")){
+        navMain.classList.remove("main-nav--closed");
+        navMain.classList.add("main-nav--opened");
+    }else{
+        navMain.classList.remove("main-nav--opened");
+        navMain.classList.add("main-nav--closed");
+    }
+})
+
+newsOpenAll.addEventListener("click", function(){
+    event.preventDefault();
+    
+    if(sectionNews.classList.contains("news__items--closed")){
+        newsOpenAll.innerHTML = "Скрыть";
+        sectionNews.classList.remove("news__items--closed");
+        sectionNews.classList.add("news__items--opened");
+    }else{
+        newsOpenAll.innerHTML = "Показать все";
+        sectionNews.classList.remove("news__items--opened");
+        sectionNews.classList.add("news__items--closed");
+    }
+})
